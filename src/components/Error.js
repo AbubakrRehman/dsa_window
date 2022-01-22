5580498cb03a7be9a4f1db9b21f6f72c5539c3c1
AbubakrRehman <abubakr.rehman91@outlook.com>
2022-01-23 03:17:55 +0530

    added 404 msg

--- src/components/Error.js
+++ src/components/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Error() {
     return (
         <h1>
-            Sorry page not found!!
+            Sorry page not found!! 404 Error
         </h1>
     )
 }
